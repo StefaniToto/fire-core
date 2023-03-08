a9198a2784c212db854f630f83225709a5adf882
Stefani Totokotsopoulou <stefani.totokocopull@visma.com>
2023-03-08 22:05:18 +0200

    Initial commit

---
+++ __.sql
@@ -0,0 +1,5 @@
+npx nx g @nrwl/angular:app admin --create admin app
+npx nx g @nrwl/angular:lib common-ui --create libs
+npx nx g @nrwl/js:lib products
+--another
+npx nx g @nrwl/angular:component banner --project=common-ui --export
---
+++ apps/share-the-house/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'share-the-house-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'share-the-house';
+}
